06d4f7390a17b6ff7af3c0de12df6b5abae944e4
wxxd-fxrest <fxrest704@gmail.com>
2024-09-21 02:35:28 +0900

    feat(security): 세션 사용자 아이디 정보 ~ Role Hierarchy

--- src/main/java/com/example/security/controller/MainController.java
+++ src/main/java/com/example/security/controller/MainController.java
@@ -1,12 +1,32 @@
 package com.example.security.controller;
 
+import java.util.Collection;
+import java.util.Iterator;
+
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+
 
 @Controller
 public class MainController {
     @GetMapping("/")
-    public String goMain() {
+    public String goMain(Model model) {
+
+        String id = SecurityContextHolder.getContext().getAuthentication().getName();
+
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+
+        Collection<? extends GrantedAuthority> authorities = authentication.getAuthorities();
+        Iterator<? extends GrantedAuthority> iter = authorities.iterator();
+        GrantedAuthority auth = iter.next();
+        String role = auth.getAuthority();
+
+        model.addAttribute("id", id);
+        model.addAttribute("role", role);
         return "main";
     }
 }
